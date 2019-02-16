80713e0d75c9605ae4841b12183c9fe6a3f05887
Robert Woodley <blwoodley@yahoo.com>
2019-02-16 16:46:19 -0600

    make it 400x400 like it is supposed to be

--- slice_cube.py
+++ slice_cube.py
@@ -9,16 +9,17 @@ def evaluator(pix):
 	pixely = k%400
 	print k
 	k += 1
-	return images[i%112].getpixel((pixelx, pixely))
+	return images[i].getpixel((pixelx, pixely))
 
 if len(sys.argv) != 2 or (sys.argv[1] != 'XSlice' and sys.argv[1] != 'YSlice'):
 	print "Usage: python slice_cube.py [XSlice|YSlice]"
 	exit()
 
-for i in range(1,113):
+for i in range(1,401):
 	j = "%03d" % (i,)
 	jpgfile = Image.open("frames/filename{}.jpg".format(j))
-	images.append(jpgfile)
+	images.append(jpgfile.copy())
+	jpgfile.close()
 
 for i in range(0,400):
 	print 'Generating frame {}'.format(i)
@@ -26,10 +27,10 @@ for i in range(0,400):
 	pixelsNew = img.load()
 	for ix in range(img.size[0]):
 		for iy in range(img.size[1]):
-			if sys.argv[1] != 'XSlice':
-				pixelsNew[ix,iy] = images[ix%112].getpixel((i, iy))
+			if sys.argv[1] == 'XSlice':
+				pixelsNew[ix,iy] = images[ix].getpixel((i, iy))
 			else:
-				pixelsNew[ix,iy] = images[ix%112].getpixel((iy, i))
+				pixelsNew[ix,iy] = images[ix].getpixel((iy, i))
 
 	j = "%03d" % (i+1,)
 	img.save("frames/out{}.jpg".format(j))
